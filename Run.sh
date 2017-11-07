7255ea4cac26d50385cefda51d3b15c66db3ac29
tgumbsch <tgumbsch@gmail.com>
2017-11-07 11:35:41 +0100

    Euler compability of linking

--- Run.sh
+++ Run.sh
@@ -6,9 +6,10 @@ chmod +x ./runTests.sh
 ./runTests.sh
 cd ../..
 
-#Link C++ to Python
+#Link C++ to Python, for some OS try line 12 instead of 13 if 12 gives an error
 g++ -std=c++14 -c -fPIC ./cpp/_main.cpp -o ./cpp/CPP.o
 g++ -shared -Wl,-install_name,./cpp/CPP.so -o ./cpp/CPP.so ./cpp/CPP.o
+# g++ -shared -Wl,-soname,./cpp/CPP.so -o ./cpp/CPP.so ./cpp/CPP.o
 
 #Unittesting of R, C++ and Python in Python
 python Unittest.py
