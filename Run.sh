1bdc9fcffe4bc8de8239533b7e85d5063ce342b1
Thomas Gumbsch <tgumbsch@gmail.com>
2018-08-08 11:45:33 +0200

    Converted to python3

--- Run.sh
+++ Run.sh
@@ -6,17 +6,17 @@ chmod +x ./runTests.sh
 ./runTests.sh
 cd ../..
 
-#Link C++ to Python, for some OS try line 12 instead of 13 if 12 gives an error
+#Link C++ to Python
 # g++ -std=c++14 -c -fPIC -fopenmp ./cpp/_main.cpp -o ./cpp/CPP.o #For Euler
 # g++ -shared -Wl,-soname,./cpp/CPP.so -o ./cpp/CPP.so ./cpp/CPP.o #For Euler
 g++ -std=c++14 -c -fPIC ./cpp/_main.cpp -o ./cpp/CPP.o
 g++ -shared -Wl,-install_name,./cpp/CPP.so -o ./cpp/CPP.so ./cpp/CPP.o
 
 #Unittesting of R, C++ and Python in Python
-python Unittest.py
+python3 Unittest.py
 
 #Runtime of wrapped calls
-python Runtime.py
+python3 Runtime.py
 
 #Logging for handling input data
-python ./py/py.py --datadir ./data --outdir ./data
+#python ./py/py.py --datadir ./data --outdir ./data
